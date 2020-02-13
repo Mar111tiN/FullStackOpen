91e8ca8a91d0fea11f7865233fe58efde70ccf59
Martin Szyska <martin.szyska37@gmail.com>
2020-02-13 11:37:30 +0100

    finished step5

--- src/index.js
+++ src/index.js
@@ -1,28 +1,46 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+
+
+const Header = ({course}) => (
+  <h1>{course}</h1>
+)
+
+const Total = ({number}) =>(
+<p>Number of exercices {number}</p>
+) 
+
+const Part = ({part}) => (
+  <p>{part.name} {part.exercises}</p>
+)
+
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course.name} />
+      <Part part={course.parts[0]} />
+      <Part part={course.parts[1]} />
+      <Part part={course.parts[2]} />
+      <Total number={course.parts.reduce((sum, part) => sum + part.exercises, 0)} />
     </div>
   )
 }
