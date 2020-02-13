97fd3a6a67e0cafbca74e653c44c1319bec99033
Martin Szyska <martinszyska37@gmail.com>
2020-02-13 12:59:38 +0100

    finished part 1c

--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 
-const Hello = ({ name, age }) => {
-  const bornYear = () => new Date().getFullYear() - age
-  
-  return (
-  <div>
-    <p>Hello {name}, you are {age} years old</p>
-  <p>So you were probably born in {bornYear()}</p>
-  </div>
-)}
-
-
 const App = () => {
-  const now = new Date();
+  const [ counter, setCounter] = useState(0);
+
+  const Display = ({ counter }) => <div>{counter}</div>
+
+  const Button = ({ onClick, lable }) => <button onClick={onClick}>{lable}</button>
+
+  const setByClick = (value) => () => setCounter(value)
+  // state change causes a re-render of the component
   return (
-  <div>
-    <h1>Greetings</h1>
-    <Hello name="Martin" age="42"/>
-  </div>
-)
-}
-ReactDOM.render(<App />, document.getElementById('root'));
+    <>
+      <Display counter={counter} />
+      <Button onClick={setByClick(counter + 1)} lable="plus" />
+      <Button onClick={setByClick(counter - 1)} lable="minus" />
+      <Button onClick={setByClick(0)} lable="reset" />
+    </>
+);
+  }
+
+ReactDOM.render(<App />, document.getElementById('root'))
