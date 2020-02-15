a5ea80835f13f90c9bbe8e69433559c48d0dfbdc
Martin Szyska <martinszyska37@gmail.com>
2020-02-15 21:27:07 +0100

    finished step 7

--- src/index.js
+++ src/index.js
@@ -3,17 +3,28 @@ import ReactDOM from 'react-dom'
 
 
 
-const Header = ({course}) => (
-  <h1>{course}</h1>
+const Header = ({ name }) => (
+  <h1>{name}</h1>
 )
 
 const Total = ({number}) =>(
-<p>Number of exercices {number}</p>
+<p><strong>total of {number} exercices </strong></p>
 ) 
 
 const Part = ({part}) => (
   <p>{part.name} {part.exercises}</p>
 )
+
+
+const Content = ({ course }) => course.parts.map(part => <Part part={part} key={part.id} />)
+
+
+const Course = ({course}) => (
+    <div>
+      <Header name={course.name} />
+      <Content course={course} />
+    </div>
+  )
 
 const App = () => {
   const course = {
@@ -21,25 +32,30 @@ const App = () => {
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id:2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id:3
+      },
+      {
+        name: 'Redux and React',
+        exercises: 10,
+        id:4
       }
     ]
   }
 
   return (
     <div>
-      <Header course={course.name} />
-      <Part part={course.parts[0]} />
-      <Part part={course.parts[1]} />
-      <Part part={course.parts[2]} />
+      <Course course={course} />
       <Total number={course.parts.reduce((sum, part) => sum + part.exercises, 0)} />
     </div>
   )
