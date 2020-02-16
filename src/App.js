f4424863fb41555bc4406257cc05f90a097feff4
Martin Szyska <martin.szyska37@gmail.com>
2020-02-16 12:01:36 +0100

    step 3 moved controlled State to component

--- src/App.js
+++ src/App.js
@@ -21,8 +21,7 @@ const App = () => {
     phone: "0178-7825900"
  },
   ]) 
-  const [ newName, setNewName ] = useState('')
-  const [ newPhone, setNewPhone ] = useState('')
+
   const [ message, setMessage ] = useState('')
 
 
@@ -34,43 +33,18 @@ const App = () => {
   </div> 
   : null
 
-
-  // INPUT control for  Form
-   const control = (setter) => (e) => setter(e.target.value)
-
-
-  const addPerson = (e) => {
-    e.preventDefault()
-    if (persons.find(person => person.name === newName)) {
-      console.log(`${newName} is already added to phonebook`)
-      setMessage(`${newName} is already added to phonebook`)
-      setTimeout(() => setMessage(''), 3000)
-    } else {
-      let addedName = {
-        name:newName,
-        phone: newPhone
-      }
-      setPersons(persons.concat(addedName))
-      setNewName('')
-      setNewPhone('')
-    }
-  }
-
-
-
-
+  
   const numbers = () => persons.map(person => <Person key={person.name[0]} person={person} />)
+
 
   return (
     <div>
       <Message message={message} />
       <h2>Phonebook</h2>
       <Form 
-        onSubmit={addPerson}
-        name={newName} 
-        onNameChange={control(setNewName)}
-        phone={newPhone}
-        onPhoneChange={control(setNewPhone)}
+        persons={persons}
+        setPersons={setPersons}
+        setMessage={setMessage}
       />
       <h2>Numbers</h2>
       {numbers()}
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,7 @@
 import React, { useState} from 'react'
 
 
-const Input = ({text, value, onChange}) => (
+const Input = ({ text, value, onChange }) => (
   <div>
     {text} : 
     <input 
@@ -11,20 +11,37 @@ const Input = ({text, value, onChange}) => (
 </div>
 )
 
-const Form = ({
-    onSubmit,
-    name,
-    onNameChange,
-    phone,
-    onPhoneChange
-  }) => {
+const Form = ({ persons, setPersons, setMessage }) => {
 
   // STATE
+  const [ newName, setNewName ] = useState('')
+  const [ newPhone, setNewPhone ] = useState('')
+
+  // INPUT control for  Form
+  const control = (setter) => (e) => setter(e.target.value)
+
+
+  const addPerson = (e) => {
+    e.preventDefault()
+    if (persons.find(person => person.name === newName)) {
+      console.log(`${newName} is already added to phonebook`)
+      setMessage(`${newName} is already added to phonebook`)
+      setTimeout(() => setMessage(''), 3000)
+    } else {
+      let addedName = {
+        name:newName,
+        phone: newPhone
+      }
+      setPersons(persons.concat(addedName))
+      setNewName('')
+      setNewPhone('')
+    }
+  }
 
   return (
-    <form onSubmit={onSubmit}>
-      <Input text="name" value={name} onChange={onNameChange} />
-      <Input text="phone" value={phone} onChange={onPhoneChange} />
+    <form onSubmit={addPerson}>
+      <Input text="name" value={newName} onChange={control(setNewName)} />
+      <Input text="phone" value={newPhone} onChange={control(setNewPhone)} />
       <div>
         <button type="submit">add</button>
       </div>
