e54a44fa7a3b92189bf355d2617e5d76c2912fdd
Martin Szyska <martinszyska37@gmail.com>
2020-02-17 07:03:37 +0100

    finished step6

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Form from './components/Form'
 import Person from './components/Person'
 import Filter from './components/Filter'
-
+import axios from 'axios'
 
 
 const App = () => {
 
   // STATE
-  const [ persons, setPersons] = useState([
-    { 
-      name: 'Arto Hellas',
-      phone: "040-1234567"
-   },
-   { 
-    name: 'Martin Szyska',
-    phone: "0178-7825900"
- },
-  ])
-
+  const [ persons, setPersons] = useState([])
   const [ message, setMessage ] = useState('')
   const [ filterPhrase, setFilterPhrase ] = useState('') 
+
+  const getData = (url) => () => {
+    axios.get(url)
+      .then(res => {
+        console.log(`received data from ${url}`)
+        setPersons(res.data)
+      })
+    }
+
+  useEffect(getData('http://localhost:3001/persons'), [])
 
 
   // HELPERS
@@ -34,7 +34,7 @@ const App = () => {
     ? persons.filter(person => person.name.toLowerCase().includes(filterPhrase) )
     : persons
 
-  const numbers = () => filteredPersons.map(person => <Person key={person.name[0]} person={person} />)
+  const numbers = () => filteredPersons.map(person => <Person key={person.name} person={person} />)
 
   const handleFilter = (e) => setFilterPhrase(e.target.value.toLowerCase())
 
