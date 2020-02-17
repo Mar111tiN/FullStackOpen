32e40458e9c7658027d27ec90f9a91931b1d41bc
Martin Szyska <martinszyska37@gmail.com>
2020-02-17 21:01:01 +0100

    finished step3

--- src/App.js
+++ src/App.js
@@ -1,16 +1,54 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+const WEATHERKEY = '0de042e62bf199731d0b5c60befc62b9'
+const weatherURL = 'http://api.weatherstack.com/current'
 
 
 
-const ShortCountry= ({ countryName, onClick }) => (
-  <div>
-    <p>{countryName}</p>
-    <button onClick={onClick(countryName)}>show</button>
-  </div>
-)
 
-const Country = ({ country, }) => (
+const ShortCountry= ({ country }) => {
+    // STATE
+    const [ showAll, setShowAll ] = useState(false)
+    
+      return (showAll)
+        ? <div>
+            <Country country={country} />
+            <button onClick={() => setShowAll(!showAll)}>hide Details</button>
+          </div>
+        :  (  
+          <div>
+            <p>{country.name}</p>
+            <button onClick={() => setShowAll(!showAll)}>show</button>
+          </div>
+        )
+  }
+
+const Country = ({ country, }) => {
+  const [weather, setWeather ] = useState({
+    temp: 'noData',
+    wind: 'noData',
+    windDir: 'noData',
+    humidity: 'noData',
+    icons: ['']
+  })
+
+  const queryURL = `${weatherURL}?access_key=${WEATHERKEY}&query=${country.capital}`
+
+  useEffect(() => {axios.get(queryURL)
+    .then(res => res.data.current)
+    .then( weather => {
+      let newWeather = {
+        temp:weather.temperature,
+        wind: weather.wind_speed,
+        windDir: weather.wind_dir,
+        humidity: weather.humidity,
+        icons: weather.weather_icons 
+      }
+      setWeather(newWeather)
+    })
+  }, [queryURL])
+
+  return (
     <div>
       <h2>{country.name}</h2>
       <p>capital: {country.capital}</p>
@@ -21,9 +59,12 @@ const Country = ({ country, }) => (
       </ul>
       <img alt={`Flag of ${country.name}`} src={country.flag} width="200" height="200"/>
       <h3>Weather in {country.capital}</h3>
+      <p><strong>temperature {weather.temp} Celsius</strong></p>
+      {weather.icons.map(icon => <img key={icon} alt="weather in {country.capital}" src={icon} width="50" height="50" />)}
+      <p><strong>wind {weather.wind} kph from {weather.windDir}</strong></p>
     </div>
   )
-
+}
 
   const Filter = ({value, onChange}) => (
     <div>Filter: <input value={value} onChange={onChange} />
@@ -50,16 +91,16 @@ const App = () => {
 
   const showCountry = (countryName) => () => setFilterPhrase(countryName)
 
-
   const filteredCountries = (filterPhrase)
     ? countries.filter((country) => country.name.toLowerCase().includes(filterPhrase))
     : countries
 
+  // Display helpers
   const showCountries = (countries) => (countries)
     ? (countries.length === 1)
       ? <Country country={countries[0]} />
       : (countries.length < 10)
-        ? countries.map(country => <ShortCountry key={country.alpha3Code} countryName={country.name} onClick={showCountry}/>)
+        ? countries.map(country => <ShortCountry key={country.alpha3Code} country={country} onClick={showCountry}/>)
         : <p>Too many countries</p>
     : <p>No countries found</p>
 
@@ -71,11 +112,6 @@ const App = () => {
       {showCountries(filteredCountries)}
     </div>
   )
-
-
-
 }
-
-
 
 export default App
