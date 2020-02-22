f2b6eec814cfbe72cd19a1502be0a8441b5422b0
Martin Szyska <martinszyska37@gmail.com>
2020-02-22 22:43:56 +0100

    building

---
+++ build/precache-manifest.82ec8f00044f6c79d5f41502c609edcf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f06b77f628ae2454ea6f7dab9019bbc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98b86a18da254517d549",
+    "url": "/static/css/main.87507a8d.chunk.css"
+  },
+  {
+    "revision": "d28c9fe466243757df0a",
+    "url": "/static/js/2.a30393cb.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.a30393cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98b86a18da254517d549",
+    "url": "/static/js/main.77a28d66.chunk.js"
+  },
+  {
+    "revision": "73cc5c71f716e545cadb",
+    "url": "/static/js/runtime-main.a70bf60b.js"
+  }
+]);
